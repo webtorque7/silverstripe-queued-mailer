6bedfbfde5a19efdea56ea8ec9da7bb3e2a182a7
Conrad <conrad@webtorque.co.nz>
2016-06-13 21:50:00 +1200

    Get correct config

--- code/Mailer/QueuedMailer.php
+++ code/Mailer/QueuedMailer.php
@@ -85,7 +85,7 @@ class QueuedMailer extends \Mailer
             /**
              * @var \WebTorque\QueuedMailer\Transport\Transport $transport
              */
-            $appIdentifier = self::config()->application_identifier;
+            $appIdentifier = \Config::inst()->get('QueuedProcessor', 'application_identifier');
             $transport = \Injector::inst()->get('MailTransport');
             $result = $transport->send(
                 $appIdentifier,
